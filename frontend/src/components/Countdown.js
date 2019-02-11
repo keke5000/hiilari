dcbf77e0407ec1f918cc44c932a33665189c67d8
LoserPanda <loserpanda82@gmail.com>
2019-02-11 20:58:21 +0200

    Add time stuff

--- frontend/src/components/Countdown.js
+++ frontend/src/components/Countdown.js
@@ -1,56 +1,122 @@
 import React, { Component } from 'react';
 
 class Countdown extends Component {
-    
-  constructor(props) {
-    super(props);
-    this.state = {
-      year: "00",
-      month: "00",
-      day: "00",
-      hour: "00",
-      minute: "00",
-      second: "00"
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            year: "00",
+            month: "00",
+            day: "00",
+            hour: "00",
+            minute: "00",
+            second: "00"
+        }
     }
-  }
 
-  componentDidMount() {
-      this.callMe();
-  }
+    componentDidMount() {
+        this.callMe();
+    }
 
-  callMe = () => {
-      setInterval(() => {
-        let time = this.countTimeDifference();
-        let beginningOfTime = new Date(0);
-      this.setState({
-        year: time.getFullYear() - beginningOfTime.getFullYear(),
-        month: time.getMonth() + 1 - beginningOfTime.getMonth(),
-        day: time.getDate() - 1 - beginningOfTime.getDate(),
-        hour: time.getHours() - beginningOfTime.getHours(),
-        minute: time.getMinutes() - beginningOfTime.getMinutes(),
-        second: time.getSeconds() - beginningOfTime.getSeconds()
-        });
-      }, 100)
-  }
+    callMe = () => {
+        setInterval(() => {
+            let time = this.countTimeDifference();
+            let beginningOfTime = new Date(0);
+            this.setState({
+                year: time.getFullYear() - beginningOfTime.getFullYear(),
+                month: time.getMonth() + 1 - beginningOfTime.getMonth(),
+                day: time.getDate() - 1 - beginningOfTime.getDate(),
+                hour: time.getHours() - beginningOfTime.getHours(),
+                minute: time.getMinutes() - beginningOfTime.getMinutes(),
+                second: time.getSeconds() - beginningOfTime.getSeconds()
+            });
+        }, 100)
+    }
 
-  countTimeDifference = () => {
-      let timeNow = new Date();
-      let doomsDay = new Date(2029, 0, 1, 1, 1, 1, 1);
-      let milliSeconds = doomsDay.valueOf() - timeNow.valueOf();
-      return new Date(milliSeconds);
-  }
-    
+    countTimeDifference = () => {
+        let timeNow = new Date();
+        let doomsDay = new Date(2029, 0, 1, 1, 1, 1, 1);
+        let milliSeconds = doomsDay.valueOf() - timeNow.valueOf();
+        return new Date(milliSeconds);
+    }
+
     render() {
         return (
             <div>
-                Time left:
+                Aikaa jäljellä +1.5 °C lämpenemiseen
                 <br />
-                {this.state.year < 10 ? "0" + this.state.year : this.state.year}:
-                {this.state.month < 10 ? "0" + this.state.month : this.state.month}:
-                {this.state.day < 10 ? "0" + this.state.day : this.state.day}:
-                {this.state.hour < 10 ? "0" + this.state.hour : this.state.hour}:
-                {this.state.minute < 10 ? "0" + this.state.minute : this.state.minute}:
-                {this.state.second < 10 ? "0" + this.state.second : this.state.second}
+                <table align="center">
+                    <tr className="countdown">
+                        <td>
+                            {this.state.year < 10 ? "0" + this.state.year : this.state.year}
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            {this.state.month < 10 ? "0" + this.state.month : this.state.month}
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            {this.state.day < 10 ? "0" + this.state.day : this.state.day}
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            {this.state.hour < 10 ? "0" + this.state.hour : this.state.hour}
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            {this.state.minute < 10 ? "0" + this.state.minute : this.state.minute}
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            {this.state.second < 10 ? "0" + this.state.second : this.state.second}
+                        </td>
+                    </tr>
+                    <tr className="countdown-second-row">
+                        <td>
+                            v
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+                            kk
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+                            pv
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+                            h
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+                            min
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+                            s
+                        </td>
+                    </tr>
+                </table>
             </div>
         );
     }
