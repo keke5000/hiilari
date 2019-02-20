c4cd0da8585d73b65f7e1478e3c0d50687ded976
LoserPanda <loserpanda82@gmail.com>
2019-02-20 19:59:59 +0200

    Add background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Countdown from './components/Countdown';
+import background from './images/Hiilibudjetti_maa.png';
+import background2 from './images/Hiilibudjetti_tausta.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header" >
           <h1>
             Hiilibudjetti
           </h1>
           <Countdown />
+          <img className="buildings" src={background} alt="Background image"/>
+          <img className="backgroundImage" src={background2} alt="Background image"/>
         </header>
       </div>
     );
