d31c0ccc567472c3e15cd054a3b434eafa47579d
keke5000 <keke5000@gmail.com>
2019-03-08 21:25:01 +0200

    infotext edit

--- frontend/src/components/Infotext.js
+++ frontend/src/components/Infotext.js
@@ -6,12 +6,16 @@ class Infotext extends Component {
       return (
         <div className="infoteksti">
           <div className="container" align="center">
+            <br/>
+            <br/>
             <p className="text-left">Hiilibudjetti havainnollistaa, millä aikavälillä nykyiset
             hiilidioksidipäästöt johtavat 1,5 asteen lämpenemiseen.</p>
+            <br/>
             <p className="text-left">
             Hallitustenvälinen ilmastonmuutospaneeli IPCC on rajannut 1,5 astetta riskirajaksi
              maapallon keskilämpötilan nousulle. Tämän rajan ylittäminen aiheuttaa
              merkittäviä vaaroja sekä luonnolle että ihmiselle.</p>
+            <br/>
             <p className="text-left">
             1,5C hiilibudjettia oli 1.1.2019 jäljellä 420 gigatonnia. Vuoden 2018 päästöt olivat 42,2 gigatonnia. Budjetti päivitetään vuosittain edeltävän vuoden toteutuneiden päästöjen mukaan.
             Hiilibudjetissa verrataan planeetan pintailman lämpötilaa vuosien 1850-1900 keskiarvoon. Laskurin lukema perustuu IPCC:n arvioon, jolla yli 1,5 asteen lämpenemiseltä välttymisen todennäköisyys on 66 prosenttia.</p>
@@ -19,12 +23,17 @@ class Infotext extends Component {
             <p className="text-left">
             Yhteenveto tutkimuksista hiilibudjetin määrästä:</p>
             <p className="text-left">
-            www.carbonbrief.org/analysis-how-much-carbon-budget-is-left-to-limit-global-warming-to-1-5c</p>
+            <a href>
+            www.carbonbrief.org/analysis-how-much-carbon-budget-is-left-to-limit-global-warming-to-1-5c
+            </a></p>
+            <br/>
             <p className="text-left">
             Lähde hiilidioksidipäästöille:
             </p>
             <p className="text-left">
-            www.globalcarbonproject.org/carbonbudget/18/infographics.htm (ei sis. maankäytön päästöjä)</p>
+            <a href>
+            www.globalcarbonproject.org/carbonbudget/18/infographics.htm (ei sis. maankäytön päästöjä)
+            </a></p>
             </div>
       </div>
     );
